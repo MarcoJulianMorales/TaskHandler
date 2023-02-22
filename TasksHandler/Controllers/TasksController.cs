5e28c525db88c46e420c0667537150d99746e461
Marco Julian Morales <marcojulian.morales@gmail.com>
2023-02-21 20:41:51 -0700

    V3.2 video 140 ok, video 141 no muestra nombre de tasks en la lista

--- TasksHandler/Controllers/TasksController.cs
+++ TasksHandler/Controllers/TasksController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TasksHandler.Entities;
 using TasksHandler.Services;
 
 namespace TasksHandler.Controllers
@@ -14,6 +16,39 @@ namespace TasksHandler.Controllers
         {
             this.applicationDbContext = applicationDbContext;
             this.usersService = usersService;
+        }
+
+        [HttpGet]
+        public async Task<List<Tasks>> Get()
+        {
+            return await applicationDbContext.Tasks.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Tasks>> Post([FromBody] string title)
+        {
+            var userId = usersService.getUserId();
+
+            var tasksExists = await applicationDbContext.Tasks.AnyAsync(t => t.UserCreatedId == userId);
+
+            var mayorOrden = 0;
+            if (tasksExists)
+            {
+                mayorOrden = await applicationDbContext.Tasks.Where(t => t.UserCreatedId == userId)
+                    .Select(t => t.Orden).MaxAsync();
+            }
+
+            var task = new Tasks
+            {
+                Tittle = title,
+                UserCreatedId = userId,
+                Creationdate = DateTime.UtcNow,
+                Orden = mayorOrden + 1
+            };
+
+            applicationDbContext.Add(task);
+            await applicationDbContext.SaveChangesAsync();
+            return task;
         }
     }
 }
---
+++ TasksHandler/wwwroot/js/Tasks.js
@@ -0,0 +1,52 @@
+﻿function addNewTask() {
+    TasksListDTO.tasks.push(new TasksElementListDTO({ id: 0, title: '' }));
+    $("[name=title-task]").last().focus();
+}
+
+async function focusoutTaskTitle(task) {
+    const title = task.title();
+    if (!title) {
+        TasksListDTO.tasks.pop();
+        return;
+    }
+    //task.id(1);
+    const data = JSON.stringify(title);
+    const response = await fetch(urlTasks, {
+        method: 'POST',
+        body: data,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        const json = await response.json();
+        task.id(json.id);
+    } else {
+        //show up error meesage
+    }
+}
+
+async function getTasks() {
+    TasksListDTO.loading(true);
+
+    const response = await fetch(urlTasks, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+            }
+    })
+
+    if (!response.ok) {
+        return;
+    }
+
+    const json = await response.json();
+    TasksListDTO.tasks([]);
+
+    json.forEach(valor => {
+        TasksListDTO.tasks.push(new TasksElementListDTO(valor));
+    });
+
+    TasksListDTO.loading(false);
+}
